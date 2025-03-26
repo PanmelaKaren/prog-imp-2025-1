27ac06be8a2585d58d3f1403ddc9a487cf1eda89
PanmelaKaren <161941633+PanmelaKaren@users.noreply.github.com>
2025-03-26 00:25:39 +0000

    exercicio vetor

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+
+public class Main {
+
+    public static int[] multVetor(int[] v) {
+        for (int i = 0; i < v.lenght; i++) {
+            v[i] *= 2; 
+        }
+        
+    }
+
+    public static void imprimeVetor(int[] v){
+        for (int i = 0; i < v.lenght; i++){
+
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int v[] = {1,3,6,2,5};
+        int resposta[];
+        resposta = multVetor(v);
+        imprimeVetor(resposta); 
+
+       
+        
+        System.out.println(div(a, b));
+    }
+}
